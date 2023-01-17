2393ccecd40740f9967355be05310f5e9e55c2f0
Laure Spake <laure.spake@otago.ac.nz>
2023-01-17 13:21:41 -0500

    base package list

---
+++ base/base-pckg-install.R
@@ -0,0 +1,31 @@
+# list of packages to install 
+
+pkg.list <- c("tidyverse", 
+              "archdata",
+              "BSDA",
+              "chemometrics",
+              "dagitty",
+              "evoarchdata",
+              "FactoMineR",
+              "factoextra",
+              "gapminder",
+              "ggdag",
+              "glm",
+              "knitr", 
+              "lubridate",
+              "lme4",
+              "nycflights13",
+              "palmerpenguins",
+              "psych",
+              "remotes",
+              "RNHANES",
+              "titanic",
+              "swirl",
+              "zooaRch"
+)
+
+install.packages(pkg.list)
+
+remotes::install_github("geanes/bioanth")
+remotes::install_github("benmarwick/binford") 
+remotes::install_github("benmarwick/evoarchdata") 
